d1d3906effb877d8d123f64db45b45ec86542da2
Vetri <vairamut@usc.edu>
2016-04-21 16:02:32 -0700

    first commit

---
+++ TomateTests/TomateTests.swift
@@ -0,0 +1,36 @@
+//
+//  TomateTests.swift
+//  TomateTests
+//
+//  Created by Vetri Selvi Vairamuthu on 4/21/16.
+//  Copyright © 2016 Vetri Selvi Vairamuthu. All rights reserved.
+//
+
+import XCTest
+@testable import Tomate
+
+class TomateTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
